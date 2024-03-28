9f2fad00a23b8b58db5487f2a3eee68b087eeaa1
gewdszdftgr <159111928+gewdszdftgr@users.noreply.github.com>
2024-03-28 15:18:03 +0800

    commit nodejs

---
+++ index.js
@@ -0,0 +1,38 @@
+import express from "express" ;
+import data1 from "./singers.json" assert{type:"json"} ;
+const {singers} = data1 ; //物件解構成陣列
+// console.log(singers);    //陣列
+
+const app = express() ;
+
+app.get("/",(req,res)=>{
+    res.send("網站首頁")
+})
+
+// /singer/:id.html
+app.get("/singer/:id.html",(req,res)=>{
+    res.send(`${req.params.id}`)
+    const {id} = req.params ; 
+    // find(v,i,arr) 
+    let result = singers.find(singer => singer.id === parseInt(id));
+    if(!result){
+        res.status(404).send("<h1>找不到歌手<>/h1");
+        return false ;
+    }
+    res.send(`<!DOCTYPE html>
+    <html lang="zh-hant-TW">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>${singer_name}的頁面</title>
+    </head>
+    <body>
+        <h1>${singer_name}的頁面</h1>
+        <img src=">${result.singer_img}" alt="">
+    </body>
+    </html>`);
+});
+
+app.listen(3000, ()=>{
+    console.log("running at http://localhost:3000");
+})
